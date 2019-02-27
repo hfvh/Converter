56a1016b4fa2a051f9e79b0a0f721ea36a388355
Stacy <anastasyago@yandex.ru>
2019-02-27 21:42:38 +0300

    fix setup

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 from setuptools import setup
 
 setup(
-    name='csv_converter',
-    version='1.0.0',
+    name='converter',
+    version='1.5.0',
     description='Command-line converter csv format to avro',
     author='Anastasiya Holubeva',
     packages=['converter'],
     entry_points={
         'console_scripts': [
-            'csv_converter=avro.converter:main',
+            'converter=converter.main:main',
         ],
     },
 )
